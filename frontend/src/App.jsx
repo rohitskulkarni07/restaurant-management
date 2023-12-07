d0c8ef84c302e9432ff6cbf85932bf60bc6059c0
Rohit Kulkarni <rohitkulkarni@Rohits-Laptop.local>
2023-12-07 17:36:20 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Register from "./components/Register";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="app-container">
+      <Register></Register>
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,44 @@
+// frontend/src/components/Login.jsx
+import { useState } from "react";
+import "../assets/styles/components/Login.css";
+
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    // Implement your login logic here
+    console.log(
+      `Logging in with username: ${username} and password: ${password}`
+    );
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Login</h2>
+      <form>
+        <label>
+          Username:
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <button type="button" onClick={handleLogin}>
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
